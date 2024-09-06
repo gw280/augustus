b66e54597980ef019a766ffc98a9845ef0b5c92c
George Wright <gw@gwright.org.uk>
2024-09-06 17:30:16 -0400

    Initial iOS project

---
+++ ios/augustus/augustus/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  augustus
+//
+//  Created by George Wright on 9/6/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/augustus/augustus/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  augustus
+//
+//  Created by George Wright on 9/6/24.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our iOS view controller
+@interface GameViewController : UIViewController
+
+@end
